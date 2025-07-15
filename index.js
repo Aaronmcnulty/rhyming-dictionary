b3b5d831e9c89d7917a318ab751ca83a3356a89a
aaronmcnulty <malcomcrabesq@outlook.com>
2025-07-15 23:05:56 +0100

    Feat: Add rhyme search from element and basic functionality

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 console.log('Hi')
+
+const rhymeSearchButton = document.getElementById('rhymeSearchButton')
+const rhymeInput = document.getElementById('rhymeInput')
+
+const formSubmit = rhymeSearchButton.addEventListener('click', (event) => {
+    event.preventDefault()
+    console.log(rhymeInput.value)
+})
