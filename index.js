1310f1106aa0e9d551397cd56a66a91117d54f51
aaronmcnulty <malcomcrabesq@outlook.com>
2025-07-16 00:45:14 +0100

    Feat: Refactor RhymeResults template to map passed in results array and display as an unordered list

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import  { RhymeResultsTemplate } from "./templates/rhymeResults.js"
 import { capitaliseFirst } from "./modules/capitalise.js"
 
+const wordsList = ['frog', 'dog', 'fog', 'slog', 'hog', 'sog', 'bog']
 
 const rhymeSearchButton = document.getElementById('rhymeSearchButton')
 const rhymeInput = document.getElementById('rhymeInput')
@@ -8,6 +9,8 @@ const rhymeInput = document.getElementById('rhymeInput')
  rhymeSearchButton.addEventListener('click', (event) => {
     event.preventDefault()
     console.log(rhymeInput.value)
-    RhymeResultsTemplate(capitaliseFirst(rhymeInput.value))
+    RhymeResultsTemplate(capitaliseFirst(rhymeInput.value), wordsList)
 })
+
+
 
--- templates/rhymeResults.js
+++ templates/rhymeResults.js
@@ -1,4 +1,4 @@
-
+import { capitaliseFirst } from "../modules/capitalise.js"
 
 export function RhymeResultsTemplate(searchWord, resultsData) {
     const rhymeResultsContainer = document.getElementById('rhymeResultsContainer')
@@ -7,6 +7,16 @@ export function RhymeResultsTemplate(searchWord, resultsData) {
     const resultsTitle = document.createElement('h3')
     resultsTitle.innerText = (`${searchWord}, rhymes with...`)
 
+    const wordResultsList = document.createElement('ul')
+
+    resultsData.map(item => {
+        const listItem = document.createElement('li')
+        listItem.innerText = capitaliseFirst(item)
+        wordResultsList.append(listItem)
+   })
+
     rhymeResultsContainer.append(resultsTitle)
+    rhymeResultsContainer.append(wordResultsList)
+
 }
 
