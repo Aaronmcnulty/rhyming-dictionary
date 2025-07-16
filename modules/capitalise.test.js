c293a7c702b7e3b5704fd98b5449dc4830154f6e
aaronmcnulty <malcomcrabesq@outlook.com>
2025-07-16 17:06:06 +0100

    Test: Add more tests to the capitaliseFirst function.

--- modules/capitalise.test.js
+++ modules/capitalise.test.js
@@ -1,11 +1,21 @@
 import { capitaliseFirst } from "./capitalise";
 
 describe('capitlaiseFirst function should', () => {
-test('Capitalises only the first letter', () => {
-  expect(capitaliseFirst('duck')).toBe('Duck');
-});
+ 
+  test('Capitalises only the first letter', () => {
+    expect(capitaliseFirst('duck')).toBe('Duck');
+  });
 
-test('Not change any letters only capitalise the first.', () => {
-    expect(capitaliseFirst('duck')).not.toBe('Goose')
-});
+  test('Not change any letters only capitalise the first.', () => {
+      expect(capitaliseFirst('duck')).not.toBe('Goose')
+  });
+
+  test('Not change the other letters to lowercase.', () => {
+      expect(capitaliseFirst('DUCK')).not.toBe('Duck')
+  })
+
+  test('Return the string unchanged if the first letter is a capital already', () => {
+      expect(capitaliseFirst('Duck')).toBe('Duck')
+  })
+
 });
