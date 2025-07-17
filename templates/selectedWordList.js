d3e2f0931fb437797285ba9fb2ccd4349cfc311f
aaronmcnulty <malcomcrabesq@outlook.com>
2025-07-17 15:32:41 +0100

    Refactor: Breakdown the selectedWordList function into seperate functions and add function notation.

--- templates/selectedWordList.js
+++ templates/selectedWordList.js
@@ -1,38 +1,66 @@
 
 export default function selectedWordList(){
 
+    //Retrieves savedWordArray from sessionStorage and parses from JSON.
     const savedWordArray = JSON.parse(sessionStorage.getItem('savedWordArray'))
 
+    //Assigns container element to variable. 
     const selectedWordsContainer = document.getElementById('selectedWordsContainer')
+    //Resets containers contents on each render.
     selectedWordsContainer.textContent = ''
 
+    //Create title element and append to the parent container.
     const savedWordsTitle = document.createElement('h3')
     savedWordsTitle.innerText = "Your Selected Rhymes"
     selectedWordsContainer.append(savedWordsTitle)
 
-        const selectedWords = document.createElement('div')
-        selectedWords.textContent = ''
-        savedWordArray.map(savedWord => {
+    //created selectedWords container. 
+    const selectedWords = document.createElement('div')
+    
+    //Map each entry from the savedWordArray and pass it to the createWordListEntry function.
+    savedWordArray.map(savedWord => {
+       createWordListEntry(savedWord)
+    })
+
+
+    function createWordListEntry(savedWord){
+    //Creates list entry element from the passed in 'savedWord' string.
             const wordListEntry = document.createElement('li')
             wordListEntry.class = 'wordListEntry'
             wordListEntry.textContent = savedWord
-
+    
+    //Creates the removeEntryButton and sets the saved word string as it's value.
             const removeEntryButton = document.createElement('button')
             removeEntryButton.textContent = 'remove'
             removeEntryButton.class = 'removeEntryButton'
             removeEntryButton.value = savedWord
 
+    //Adds event listener which calls the removeWordFromArray function when clicked.. 
             removeEntryButton.addEventListener('click', function(event){
-              const filteredArray = savedWordArray.filter(word => {
-                    if(word !== removeEntryButton.value){
-                        return word
-                    }
-                })
-                sessionStorage.setItem('savedWordArray', JSON.stringify(filteredArray))
-                selectedWordList()
+                removeWordFromArray(event.target.value)
             })
-            selectedWordsContainer.append(selectedWords)
-            selectedWords.append(wordListEntry)
+
+    //Appends created elements to their respective parent elements.
             wordListEntry.append(removeEntryButton)
+            selectedWords.append(wordListEntry)
+            selectedWordsContainer.append(selectedWords)
+            
+        }
+        
+//Filters the targetWord from the array and passes this filteredArray to the 'setSavedWordArray' function.
+    function removeWordFromArray(targetWord){
+        const filteredArray = savedWordArray.filter(word => {
+            if(word !== targetWord){
+                return word
+            }
         })
+        setSavedWordArray(filteredArray)
     }
+
+//Overwrites the 'savedWordArray' in session storage with the passed in 'updatedArray'.
+    function setSavedWordArray(updatedArray){
+        sessionStorage.setItem('savedWordArray', JSON.stringify(updatedArray))
+        selectedWordList()
+    }
+   
+}
