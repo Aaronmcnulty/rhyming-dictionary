4dde7aca1605f72d505fd594f922d8fcd0c96f01
aaronmcnulty <malcomcrabesq@outlook.com>
2025-07-31 14:49:29 +0100

    Test: add further tests to 'getRandomInt.js'.

--- modules/getRandomInt/getRandomInt.test.js
+++ modules/getRandomInt/getRandomInt.test.js
@@ -7,9 +7,24 @@ describe('getRandomInt', () => {
     expect(typeof randomNum).toBe('number');
   });
 
-  test('Should return a number', () => {
+  test('Should NOT return a string', () => {
     const randomNum = getRandomInt(10)
     expect(typeof randomNum).not.toBe('string')
+  });
+
+  test('Should return a number less or equal to the input argument', () => {
+    const randomNum = getRandomInt(1)
+    expect(randomNum <= 1).toBe(true)
+  });
+
+  test('Should NOT return a number greater than the input argument', () => {
+    const randomNum = getRandomInt(1)
+    expect(randomNum > 1).toBe(false)
+  });
+
+  test('Returned number should be greater or equal to 0', () => {
+    const randomNum = getRandomInt(1)
+    expect(randomNum > -1).toBe(true)
   });
 
 });
